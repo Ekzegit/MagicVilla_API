3206f23c6e29a656a69ba47014dd94b37f2a4463
Ekzegit <exe.acev@hotmail.com>
2023-05-28 13:05:31 -0400

    DbContext - Inyeccion de dependencias

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿
+using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
+        {
+            
+        }
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                
+            new Villa()
+            {
+                Id = 1,
+                Nombre = "Villa Real",
+                Detalle = "Detalle de la villaa...",
+                ImagenUrl = "",
+                Ocupantes = 5,
+                MetrosCuadrados = 50,
+                Tarifa = 200,
+                Amenidad = "",
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now,
+
+
+            },
+            new Villa()
+            {
+                Id = 2,
+                Nombre = "Villa Santos",
+                Detalle = "Detalle de la villaa...",
+                ImagenUrl = "",
+                Ocupantes = 3,
+                MetrosCuadrados = 30,
+                Tarifa = 100,
+                Amenidad = "",
+                FechaCreacion = DateTime.Now,
+                FechaActualizacion = DateTime.Now,
+
+
+            }
+            );
+        }
+    }
+}
